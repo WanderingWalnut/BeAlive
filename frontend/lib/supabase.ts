85072438952d347cef47536042209c03c46e72ce
Saad Sheikh <sheikh.saad.aqeel@gmail.com>
2025-10-04 17:33:24 -0600

    fix auth

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -3,7 +3,7 @@ import 'react-native-get-random-values';
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://lqmlvuwidtgppfwzzorp.supabase.co/';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxbWx2dXdpZHRncHBmd3p6b3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1OTUyOTQsImV4cCI6MjA3NTE3MTI5NH0.LLX1mkc1qFPNhffbWqZ4XXXuLT1g8xNg2FjEt4t3Gt4 ';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxbWx2dXdpZHRncHBmd3p6b3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1OTUyOTQsImV4cCI6MjA3NTE3MTI5NH0.LLX1mkc1qFPNhffbWqZ4XXXuLT1g8xNg2FjEt4t3Gt4';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
